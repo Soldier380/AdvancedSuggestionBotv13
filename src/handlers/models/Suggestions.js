2b8faabc9435ee109af28dd46f37cf0c320138cb
Soldier380 <75277509+Soldier380@users.noreply.github.com>
2022-08-21 20:48:27 -0500

    Create Suggestions.js

---
+++ src/handlers/models/Suggestions.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+
+const NoteSchema = new Schema({
+  
+  suggestion_id: { type: String, required: true },
+  upvotes: { type: Number, required: false, default: 0},
+    downvotes: { type: Number, required: false, default: 0},
+    down_users: { type: Array, required: false},
+    up_users: { type: Array, required: false},
+    status: { type: String, required: false }
+})
+
+module.exports = mongoose.model('SpacexSuggestions', NoteSchema );

951fe7b47aa9447c8e656ab320a9ed0007038661
Soldier380 <75277509+Soldier380@users.noreply.github.com>
2022-10-04 20:37:55 -0500

    Update setup-suggest.js

--- src/slash/Suggestions/setup-suggest.js
+++ src/slash/Suggestions/setup-suggest.js
@@ -9,16 +9,16 @@ module.exports = {
     botPerms: ["ADMINISTRATOR"],
     userPerms: ["ADMINISTRATOR"],
   options: [{name:"canal",description:"Canal de sugerencias a establecer",required:true,type:"CHANNEL"}]
-    run: async (client, message, args) => {
+    run: async (client, interaction) => {
    const ch = interaction.options.getChannel("canal")
    if(!ch.type === "GUILD_TEXT") return interaction.reply("Este no es un canal de texto")
       
-let data = await GuildConfig.findOne({ guildId: message.guild.id })
+let data = await GuildConfig.findOne({ guildId: interaction.guild.id })
   if(!data){
-    const newdata = new GuildConfig({ guildId: message.guild.id, suggestionChannel: ch.id })
+    const newdata = new GuildConfig({ guildId: interaction.guild.id, suggestionChannel: ch.id })
     newdata.save()
   } else {
-    await GuildConfig.findOneAndUpdate({ guildId: message.guild.id }, { suggestionChannel: ch.id })
+    await GuildConfig.findOneAndUpdate({ guildId: interaction.guild.id }, { suggestionChannel: ch.id })
   }
 return interaction.reply(`:white_check_mark: Canal de sugerencias establecido ${ch}`)
     }
